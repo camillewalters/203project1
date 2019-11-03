272c8faec7a03832ae8bcd4ad1065caecd2ebd0c
camillewalters <camille.h.walters@gmail.com>
2019-11-03 14:14:51 -0500

    Add finding steepest slope

--- oneb.m
+++ oneb.m
@@ -6,5 +6,22 @@ z=0.00125.*exp(-((x-3).^2+0.5.*y.^2)).*(sin(2.*x)+2.*sin(0.75*(0.5*y-2).^2)).*(1
 
 dx=diff(z,x)
 dy=diff(z,y)
+gradient(x,y)=sqrt(dx.^2+dy.^2)
 
+maxslope=0;
+maxx=0;
+maxy=0;
+
+for i= 1:0.05:5.5
+    for j= -4:0.05:3
+        if gradient(i,j)>maxslope
+            maxslope=double(gradient(i,j));
+            maxx=i;
+            maxy=j;
+        end
+    end
+end
+
+disp(maxx)
+disp(maxy)
 
