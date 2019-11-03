fde4a4c737e9962551e96341af106b69a6209ec3
camillewalters <camille.h.walters@gmail.com>
2019-11-03 08:13:29 -0500

    Add more files

--- oneb.m
+++ oneb.m
@@ -2,12 +2,9 @@ clear all;
 clc;
 
 syms x y;
-% assume(x>1&x<5.5);
-% assume(y>-4&y<3);
 z=0.00125.*exp(-((x-3).^2+0.5.*y.^2)).*(sin(2.*x)+2.*sin(0.75*(0.5*y-2).^2)).*(16.*x+64.*x.^2+y.^2);
 
-dx=diff(z,x)==0
-dy=diff(z,y)==0;
-sol=solve([dx,dy]);
-xsol=sol.x
-ysol=sol.y
+dx=diff(z,x)
+dy=diff(z,y)
+
+
