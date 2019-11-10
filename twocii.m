dc85bc4f0390c898588ea82b1e3162379ee1c69c
camillewalters <camille.h.walters@gmail.com>
2019-11-10 11:08:35 -0500

    i think im done?

--- twocii.m
+++ twocii.m
@@ -6,9 +6,9 @@ syms x y z
 t=-0.1.*z.^2+17*exp(-0.1.*((0.1.*x-2)-(0.05.*y-1).^2-(z-1).^2))-10;
 
 %components of the gradient of the temperature function
-dx(x,y,z)=diff(t,x);
-dy(x,y,z)=diff(t,y);
-dz(x,y,z)=diff(t,z);
+dx(x,y,z)=diff(t,x)
+dy(x,y,z)=diff(t,y)
+dz(x,y,z)=diff(t,z)
 
 %northwest direction
 i1=-1./(sqrt(1+1+1.3469.^2));
@@ -19,6 +19,7 @@ k1=1.3469./(sqrt(1+1+1.3469.^2));
 i2=-1./(sqrt(1+1+0.7912.^2));
 j2=-1./(sqrt(1+1+0.7912.^2));
 k2=-0.7912./(sqrt(1+1+0.7912.^2));
+
 
 %directional derivative north west
 ddnw=double((i1.*dx(4,-0.3,0.1675))+(j1.*dy(4,-0.3,0.1675))+(j1.*dz(4,-0.3,0.1675)))
